d8aa1a1c9787a84bb7fa6185082ba991ce76576c
Shiba Murmu <shibamurmu001@gmail.com>
2026-01-02 19:24:35 +0530

    new svg code added for better visibility

--- src/component/header/Navbar.jsx
+++ src/component/header/Navbar.jsx
@@ -7,9 +7,13 @@ function Navbar() {
     }, [])
     return (
         <>
-            <div className='bg-(--color-primary) text-white font-domine p-4 flex items-center h-16 md:h-20'>
+            <div className='bg-(--color-primary) text-white font-domine p-4 flex items-center h-16 md:h-20 gap-1  md:gap-3'>
                 <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" className='bg-()' viewBox="0 -960 960 960" width="24px" fill='currentColor'><path d="m384-336 56-57-87-87 87-87-56-57-144 144 144 144Zm192 0 144-144-144-144-56 57 87 87-87 87 56 57ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z" /></svg>
+                    <svg width="40px" height="40px" className='fill-(--color-button-background) rounded-2xl' fill='currentColor' viewBox="0 0 16 16"  xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd" clipRule="evenodd" d="M15 1H1V15H15V1ZM6 5L7.41421 6.41421L5.82843 8L7.41421 9.58579L6 11L3 8L6 5ZM10 5L8.58579 6.41421L10.1716 8L8.58579 9.58579L10 11L13 8L10 5Z" fill=''  />
+                    </svg>
+
+
                 </span>
                 <span className='text-2xl '>{websiteHeading}</span>
             </div>
