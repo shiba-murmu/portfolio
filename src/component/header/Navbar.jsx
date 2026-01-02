b0e7426423d2724c3fadef58ccd185bee0268f7b
Shiba Murmu <shibamurmu001@gmail.com>
2026-01-02 18:47:50 +0530

    New font is added to the project

--- src/component/header/Navbar.jsx
+++ src/component/header/Navbar.jsx
@@ -3,11 +3,11 @@ import React ,{useEffect, useState} from 'react'
 function Navbar() {
     const [websiteHeading, setWebsiteHeading] = useState(null)
     useEffect(() => {
-        setWebsiteHeading('Global PortFolio')
+        setWebsiteHeading('DevFolio.io')
     }, [])
     return (
         <>
-            <div className='bg-gray-800 text-white ubuntu-bold p-4 flex items-center h-16 md:h-20'>
+            <div className='bg-(--color-primary) text-white font-domine p-4 flex items-center h-16 md:h-20'>
                 <span className='text-2xl '>{websiteHeading}</span>
             </div>
     </>
