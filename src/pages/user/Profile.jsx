7f1551ad6d2cc1b8131ba991ce985474a9c08854
Shiba Murmu <shibamurmu001@gmail.com>
2026-01-03 20:20:05 +0530

    profile code earesed

--- src/pages/user/Profile.jsx
+++ src/pages/user/Profile.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 function Profile() {
-  return (
-    <div>Profile</div>
-  )
+    return (
+        <>
+            <div className='text-black'>
+                Designing pending
+            </div>
+        </>
+    )
 }
 
 export default Profile
