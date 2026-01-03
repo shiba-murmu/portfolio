33b528be64969be191eafef740aeade5a3753ae5
Shiba Murmu <shibamurmu001@gmail.com>
2026-01-03 19:40:34 +0530

    one more routes added, in the code

--- src/All_router.jsx
+++ src/All_router.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Navbar from './component/header/Navbar'
+import Profile from './pages/user/Profile'
 function All_router() {
     const [user, setUser] = useState('original')
     return (
@@ -10,6 +11,7 @@ function All_router() {
             <Routes>
                 <Route path="/" element={<Navigate to={`/portfolio/${user}`} />} />
                 <Route path='/portfolio/:name' element={<Home />} />
+                <Route path='/profile/:username' element={<Profile />} />
             </Routes>
         </>
     )
