625901f8a2cfb2328c7db753b6697ea09b1963a4
Shiba Murmu <shibamurmu001@gmail.com>
2026-01-08 21:32:04 +0530

    routes added upto adminLogin page .

--- src/All_router.jsx
+++ src/All_router.jsx
@@ -4,21 +4,22 @@ import { useLocation } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Navbar from './component/header/Navbar'
 import Profile from './pages/user/Profile'
+import AdminLogin from './admin/AdminLogin'
 function All_router() {
     const location = useLocation();
      // routes where navbar should be hidden
-  const hideNavbarRoutes = ["/portfolio-requesting"];
+  const hideNavbarRoutes = ["/portfolio-requesting" , '/admin/login'];
 
   const shouldHideNavbar = hideNavbarRoutes.some((path) =>
     location.pathname.startsWith(path)
   );
     return (
         <>
-            {shouldHideNavbar && <Navbar />}
+            {shouldHideNavbar && <Navbar /> }
             <Routes>
                 <Route path="/" element={<Navigate to={`/portfolio-requesting`} />} />
                 <Route path='/portfolio-requesting' element={<Home />} />
-
+                <Route path='/admin/login' element={<AdminLogin />} />
                 <Route path='/profile/:username' element={<Profile />} />
             </Routes>
         </>
